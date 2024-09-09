a1d8c4a38e404d39b2beceb6ff826b32f54f4b38
konadaa <carulaa@icloud.com>
2024-09-09 19:53:56 +0200

    Update terminal.cpp

--- drivers/terminal.cpp
+++ drivers/terminal.cpp
@@ -1,17 +1,29 @@
 #include "terminal.h"
-#include <stdint.h>
 #include <cstddef>
-#include <cstdint>
 
-static uint16_t terminal_row = 0;
-static uint16_t terminal_column = 0;
-static uint8_t terminal_color = 0x0F;
+static uint8_t terminal_row;
+static uint8_t terminal_column;
+static uint8_t terminal_color;
+static uint16_t* terminal_buffer;
+
+static inline uint16_t vga_entry(unsigned char c, uint8_t color) {
+    return (uint16_t) c | (uint16_t) color << 8;
+}
+
+static inline uint8_t vga_entry_color(uint8_t fg, uint8_t bg) {
+    return fg | bg << 4;
+}
 
 void terminal_initialize() {
-    for (uint16_t y = 0; y < VGA_HEIGHT; y++) {
-        for (uint16_t x = 0; x < VGA_WIDTH; x++) {
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_entry_color(7, 0); 
+    terminal_buffer = const_cast<uint16_t*>(VGA_MEMORY);
+
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
             const size_t index = y * VGA_WIDTH + x;
-            VGA_MEMORY[index] = (terminal_color << 8) | ' ';
+            terminal_buffer[index] = vga_entry(' ', terminal_color); 
         }
     }
 }
@@ -22,11 +34,24 @@ void terminal_putchar(char c) {
         terminal_column = 0;
     } else {
         const size_t index = terminal_row * VGA_WIDTH + terminal_column;
-        VGA_MEMORY[index] = (terminal_color << 8) | c;
+        terminal_buffer[index] = vga_entry(c, terminal_color);
         terminal_column++;
+
         if (terminal_column >= VGA_WIDTH) {
             terminal_column = 0;
             terminal_row++;
+        }
+
+        if (terminal_row >= VGA_HEIGHT) {
+            for (size_t y = 1; y < VGA_HEIGHT; y++) {
+                for (size_t x = 0; x < VGA_WIDTH; x++) {
+                    terminal_buffer[(y - 1) * VGA_WIDTH + x] = terminal_buffer[y * VGA_WIDTH + x];
+                }
+            }
+            for (size_t x = 0; x < VGA_WIDTH; x++) {
+                terminal_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH + x] = vga_entry(' ', terminal_color);
+            }
+            terminal_row = VGA_HEIGHT - 1;
         }
     }
 }
@@ -36,3 +61,4 @@ void terminal_write(const char* str) {
         terminal_putchar(str[i]);
     }
 }
+
